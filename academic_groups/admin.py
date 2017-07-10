232bf4942d235542d5751083296b22b74cd01640
Alex <kis.alex97@yandex.ru>
2017-07-10 21:04:21 +0300

    add filter for educational_form

--- academic_groups/admin.py
+++ academic_groups/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class StudentInline(admin.TabularInline):
+    model = models.Student
+    extra = 0
+
+
+class ExamInline(admin.TabularInline):
+    model = models.Exam
+    extra = 0
+
+
+class EventInline(admin.TabularInline):
+    model = models.Event
+    extra = 0
+
+
+class AcademicGroupAdmin(admin.ModelAdmin):
+    list_display = [
+        'academic_group_name',
+        'course',
+        'institute',
+        'student_count'
+    ]
+
+    list_filter = [
+        'institute',
+    ]
+
+    search_fields = [
+        'academic_group_name',
+    ]
+
+    inlines = [
+        StudentInline,
+    ]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        'student_name',
+        'academic_group',
+        'educational_form',
+    ]
+
+    list_filter = [
+        'academic_group',
+        'educational_form',
+    ]
+
+    search_fields = [
+        'student_name',
+        'academic_group',
+    ]
+
+    inlines = [
+        ExamInline,
+        EventInline,
+    ]
+
+
+admin.site.register(models.AcademicGroup, AcademicGroupAdmin)
+admin.site.register(models.Student, StudentAdmin)
