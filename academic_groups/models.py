4dbb73fa84db61a6d2564a30e4d9f314833d293d
Alex <kis.alex97@yandex.ru>
2017-07-10 15:50:29 +0300

    Cretate models: AcademicGroup, Student, Exam, Event

--- academic_groups/models.py
+++ academic_groups/models.py
@@ -17,10 +17,20 @@ class Student(models.Model):
         'бюджет',
         'контракт',
     ])
-    average_score = models.FloatField()
 
 
 class Exam(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     exam_name = models.CharField(max_length=50)
     score = models.IntegerField(default=0)
+
+
+class Event(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=200)
+    event_area = models.CharField(max_length=200, choices=[
+        ('kmd', 'Культурно-массовая деятельность'),
+        ('sd', 'Спортивная деятельность'),
+        ('nd', 'Научная деятельность'),
+        ('od', 'Общественная деятельность'),
+    ])
