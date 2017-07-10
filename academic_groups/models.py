d6c1f2a1b3dc4e6f6a1569a1344e6b1e0bd4041f
Alex <kis.alex97@yandex.ru>
2017-07-10 21:03:32 +0300

    Attributes: starosta_email, starosta_phone_number added

--- academic_groups/models.py
+++ academic_groups/models.py
@@ -4,25 +4,36 @@ from django.db import models
 class AcademicGroup(models.Model):
     institute = models.CharField(max_length=200)
     course = models.PositiveSmallIntegerField()
-    academic_group = models.CharField(max_length=6)
+    academic_group_name = models.CharField(max_length=6)
     starosta = models.CharField(max_length=50)
+    starosta_email = models.CharField(max_length=50)
+    starosta_phone_number = models.CharField(max_length=12)
     curator = models.CharField(max_length=50)
     student_count = models.IntegerField()
 
+    def __str__(self):
+        return self.academic_group_name
+
 
 class Student(models.Model):
-    application = models.ForeignKey(Application, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    educational_form = models.CharField([
-        'бюджет',
-        'контракт',
+    academic_group = models.ForeignKey(AcademicGroup, on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=50)
+    educational_form = models.CharField(max_length=8, choices=[
+        ('b', 'бюджет'),
+        ('k', 'контракт'),
     ])
+
+    def __str__(self):
+        return self.student_name
 
 
 class Exam(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     exam_name = models.CharField(max_length=50)
     score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.exam_name
 
 
 class Event(models.Model):
@@ -34,3 +45,6 @@ class Event(models.Model):
         ('nd', 'Научная деятельность'),
         ('od', 'Общественная деятельность'),
     ])
+
+    def __str__(self):
+        return self.event_name
