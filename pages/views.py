469ef89f6fea6bb0ba4231cb7d3f53f1327002fe
Alex <kis.alex97@yandex.ru>
2017-07-13 13:55:39 +0300

    Use decorater @login_required

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 
+@login_required(login_url='/auth/log_in')
 def home(request):
     user = request.user
-    if user.is_authenticated:
-        context = {
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-        }
 
-        return render(request, 'pages/home.html', context)
-    else:
-        return redirect('/auth/log_in')
+    context = {
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+    }
+
+    return render(request, 'pages/home.html', context)
